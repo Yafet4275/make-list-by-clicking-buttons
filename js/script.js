0498b2cb2aa00d107db62716d0d5eb82e4ede310
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2023-04-20 17:04:12 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const itemList = document.getElementById('itemList');
+
+button1.addEventListener('click', function() {
+  const item = 'Button 1';
+  const li = document.createElement('li');
+  li.textContent = item;
+  itemList.appendChild(li);
+});
+
+button2.addEventListener('click', function() {
+  const item = 'Button 2';
+  const li = document.createElement('li');
+  li.textContent = item;
+  itemList.appendChild(li);
+});
+
+button3.addEventListener('click', function() {
+  const item = 'Button 3';
+  const li = document.createElement('li');
+  li.textContent = item;
+  itemList.appendChild(li);
+});
