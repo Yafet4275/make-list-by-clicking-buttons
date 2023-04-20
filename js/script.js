d3f3e86124d3fb96ca346e3c1b1720be07f31d41
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2023-04-20 17:42:08 -0500

    update javascript
    better code

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
+// const button1 = document.getElementById('button1');
+// const button2 = document.getElementById('button2');
+// const button3 = document.getElementById('button3');
+// const itemList = document.getElementById('itemList');
+
+// button1.addEventListener('click', function() {
+//   const item = 'Button 1';
+//   const li = document.createElement('li');
+//   li.textContent = item;
+//   itemList.appendChild(li);
+// });
+
+// button2.addEventListener('click', function() {
+//   const item = 'Button 2';
+//   const li = document.createElement('li');
+//   li.textContent = item;
+//   itemList.appendChild(li);
+// });
+
+// button3.addEventListener('click', function() {
+//   const item = 'Button 3';
+//   const li = document.createElement('li');
+//   li.textContent = item;
+//   itemList.appendChild(li);
+// });
+
+const buttons = document.getElementById('buttons');
 const itemList = document.getElementById('itemList');
 
-button1.addEventListener('click', function() {
-  const item = 'Button 1';
-  const li = document.createElement('li');
-  li.textContent = item;
-  itemList.appendChild(li);
-});
-
-button2.addEventListener('click', function() {
-  const item = 'Button 2';
-  const li = document.createElement('li');
-  li.textContent = item;
-  itemList.appendChild(li);
-});
-
-button3.addEventListener('click', function() {
-  const item = 'Button 3';
-  const li = document.createElement('li');
-  li.textContent = item;
-  itemList.appendChild(li);
+buttons.addEventListener('click', function(event) {
+  if (event.target.classList.contains('item-button')) {
+    const item = event.target.textContent;
+    const li = document.createElement('li');
+    li.textContent = item;
+    itemList.appendChild(li);
+  }
 });
